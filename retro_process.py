3aa7e9d326b641e56c088b555a36829d577790c5
Zl He <hzlclock@gmail.com>
2020-10-12 19:57:56 +0800

    fixed a unicode bug

--- retro_process.py
+++ retro_process.py
@@ -6,7 +6,8 @@ import os, sys
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--dir", help='Specify a folder path. For example, `--dir "C:\Users\heber\Pictures"`. Note that a "processed" folder will be created and be used to store processed files.', required=True)
+parser.add_argument("--dir",help='''Specify folder path. For example, --dir "C:\\Users\\heber\\Pictures".
+                    Note that a "processed" folder will be created and be used to store processed files.''',required=True)
 args = parser.parse_args()
 
 
