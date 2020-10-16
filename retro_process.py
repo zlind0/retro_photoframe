810784c9bc3d09e80fe0e59c8c46dfa46857cd67
zhe <hzlclock@gmail.com>
2020-10-16 20:46:51 +0800

    add mono indicator

--- retro_process.py
+++ retro_process.py
@@ -3,18 +3,11 @@ from PIL import ImageFont
 from PIL import ImageDraw
 from PIL.ExifTags import TAGS
 import os, sys
-
+from multiprocessing import Pool,cpu_count
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--dir",help='''Specify folder path. For example, --dir "C:\\Users\\heber\\Pictures".
-                    Note that a "processed" folder will be created and be used to store processed files.''',required=True)
-args = parser.parse_args()
 
 
-processeddir=os.path.join(args.dir, "processed")
-
-def progressbar(it, prefix="", size=60, file=sys.stdout):
-    count = len(it)
+def progressbar(it, prefix="", size=60, file=sys.stdout, count=1):
     def show(j):
         x = int(size*j/count)
         file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
@@ -26,37 +19,86 @@ def progressbar(it, prefix="", size=60, file=sys.stdout):
     file.write("\n")
     file.flush()
 
-for file in progressbar(os.listdir(args.dir)):
+def processone(packedarg):
+    file, processeddir, args=packedarg
     try:
         if not os.path.exists(processeddir):
             os.mkdir(processeddir)
-        if os.path.exists(os.path.join(processeddir, file)):
-            continue
 
         img = Image.open(os.path.join(args.dir, file))
-        font = ImageFont.truetype("retro.ttf", int(img.size[0]/30))
         exifs={}
         for (k,v) in img._getexif().items():
             exifs[TAGS.get(k)]=v
 
+        file=exifs["DateTimeDigitized"].replace(":","")+".JPG"
+        if os.path.exists(os.path.join(processeddir, file)):
+            return
+
+        font = ImageFont.truetype("retro.ttf", int(img.size[0]/30))
+        monofont = ImageFont.truetype("mono.ttf", int(img.size[0]/60))
+        
+
         iso=exifs["ISOSpeedRatings"]
-        focallen=int(exifs["FocalLength"][0]/exifs["FocalLength"][1])
-        shutter=exifs["ExposureTime"][0]/exifs["ExposureTime"][1]
+        try:
+            focallen="%3.0f"%(exifs["FocalLength"][0]/exifs["FocalLength"][1])
+            shutter=exifs["ExposureTime"][0]/exifs["ExposureTime"][1]
+            fnum=exifs["FNumber"][0]/exifs["FNumber"][1]
+        except:
+            focallen="%3.0f"%(exifs["FocalLength"])
+            shutter=exifs["ExposureTime"]
+            fnum=exifs["FNumber"]
+        
         shottime=exifs["DateTimeDigitized"].replace(":"," ")
-        fnum=str(exifs["FNumber"][0]/exifs["FNumber"][1]).replace(".0","")
-        if shutter<0.3: 
-            shutter="1/"+str(int(1/shutter))
+        
+        if fnum>9:
+            fnum="%-3.0f"%(fnum)
         else:
-            shutter=str(shutter)+'"'
+            fnum="%1.1f"%(fnum)
+
+        if shutter<0.3: 
+            shuttertext="S/"
+            shutter="%-4.0f"%(1/shutter)
+        else:
+            shuttertext="S "
+            shutter="%d\""%shutter
+        shutter=" "*(4-len(shutter))+shutter
+
         borderwidth=int(min(img.size)*0.03)
-        textheight=int(img.size[0]*0.04)
-        newim = Image.new(mode = "RGB", size=(int(img.size[0])+borderwidth*2, int(img.size[1])+borderwidth+textheight))
+        textheight=int(img.size[0]*0.06)
+        newim = Image.new(mode = "RGB", size=(int(img.size[0])+borderwidth*2, int(img.size[1])+borderwidth+textheight), color=(20,20,20))
+
         newim.paste(img, (borderwidth, borderwidth))
         draw = ImageDraw.Draw(newim)
-        draw.text((borderwidth+img.size[0]*0.01, img.size[1]+textheight*0.5+borderwidth*0.5),f"{focallen}mm {shutter}  F{fnum}  ISO {iso}",(204, 255, 51),font=font)
-        draw.text((borderwidth+img.size[0]*0.70, img.size[1]+textheight*0.5+borderwidth*0.5),f"{shottime}",(204, 255, 51),font=font)
-        draw.text((borderwidth+img.size[0]*0.50, img.size[1]+textheight*0.5+borderwidth*0.5),"LindoHe",(255, 102, 102),font=font)
-        newim.save(os.path.join(processeddir, file), quality=95)
-    except: pass
+
+        digital_text_y=img.size[1]+textheight*0.5+borderwidth*0.5
+        mono_text_y=img.size[1]+textheight*0.3+borderwidth*0.5
+        draw.text((borderwidth+img.size[0]*0.06, mono_text_y),f"mm {shuttertext}      f/     ISO",(204, 255, 51),font=monofont)
+        draw.text((borderwidth+img.size[0]*0.01, digital_text_y),f"{focallen}",(204, 255, 51),font=font)
+        draw.text((borderwidth+img.size[0]*0.11, digital_text_y),f"{shutter}",(204, 255, 51),font=font)
+        draw.text((borderwidth+img.size[0]*0.20, digital_text_y),f"{fnum}",(204, 255, 51),font=font)
+        draw.text((borderwidth+img.size[0]*0.30, digital_text_y),f"{iso}",(204, 255, 51),font=font)
+        draw.text((borderwidth+img.size[0]*0.65, mono_text_y),f"DATE",(204, 255, 51),font=monofont)
+        draw.text((borderwidth+img.size[0]*0.70, digital_text_y),f"{shottime}",(204, 255, 51),font=font)
+        draw.text((borderwidth+img.size[0]*0.45, digital_text_y),args.author,(255, 102, 102),font=font)
+        newim.save(os.path.join(processeddir, file), quality=args.quality)
+    except Exception as e: 
+        print(e)
+        pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir",help='''Specify folder path. For example, --dir "C:\\Users\\heber\\Pictures".
+                        Note that a "processed" folder will be created and be used to store processed files.''',required=True)
+    parser.add_argument("--author",help='''Who you are.''',default="LindoHe")
+    parser.add_argument("--quality",help='''Quality of JPEG''',default=95, type=int)
+    args = parser.parse_args()
+    processeddir=os.path.join(args.dir, f"author_{args.author}")
+    packedargs=[]
+    for file in os.listdir(args.dir):
+        packedargs.append((file, processeddir, args))
+    with Pool(processes=cpu_count(), maxtasksperchild=1) as po:
+        res=po.imap(processone, packedargs)
+        for _ in progressbar(res, count=len(packedargs)):
+            pass
     
     
